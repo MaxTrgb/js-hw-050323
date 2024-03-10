e29fc245f2704f5c8ac33b04302ad0ffa599e829
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2024-03-10 20:39:28 +0200

    jq

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,21 @@
+$(document).ready(function() {
+    $('#form').submit(function(event) {
+        event.preventDefault(); 
+        const length = parseInt($('#lengthInput').val());
+        const includeNumeric = $('#numericCheckbox').prop('checked');
+        const includeUppercase = $('#uppercaseCheckbox').prop('checked');
+        const includeLowercase = $('#lowercaseCheckbox').prop('checked');
 
-const form = document.querySelector("#form");
-const lengthInput = form.querySelector("#lengthInput");
-const numericCheckbox = form.querySelector("#numericCheckbox");
-const uppercaseCheckbox = form.querySelector("#uppercaseCheckbox");
-const lowercaseCheckbox = form.querySelector("#lowercaseCheckbox");
-const resultInput = form.querySelector("#resultInput");
+        let charset = '';
+        if (includeNumeric) charset += '0123456789';
+        if (includeUppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        if (includeLowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
 
+        let result = '';
+        for (let i = 0; i < length; i++) {
+            result += charset.charAt(Math.floor(Math.random() * charset.length));
+        }
 
-function generateRandomString(length, includeNumeric, includeUppercase, includeLowercase) {
-    let charset = '';
-    if (includeNumeric) charset += '0123456789';
-    if (includeUppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (includeLowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
-    
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        result += charset.charAt(Math.floor(Math.random() * charset.length));
-    }
-    return result;
-}
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    const length = parseInt(lengthInput.value);
-    const includeNumeric = numericCheckbox.checked;
-    const includeUppercase = uppercaseCheckbox.checked;
-    const includeLowercase = lowercaseCheckbox.checked;
-    
-    
-    const randomString = generateRandomString(length, includeNumeric, includeUppercase, includeLowercase);
-    resultInput.value = randomString;
+        $('#resultInput').val(result);
+    });
 });
